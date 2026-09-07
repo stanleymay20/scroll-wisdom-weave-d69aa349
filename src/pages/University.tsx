f80fb1d490b7c3a592b0a58a20c03c7fe4b36c0f
Stanley Osei-Wusu <107648937+stanleymay20@users.noreply.github.com>
2026-09-07 02:13:11 +0200

    feat(university): wire academic administration into university workspace

--- src/pages/University.tsx
+++ src/pages/University.tsx
@@ -1,3 +1,27 @@
+import { Link, useSearchParams } from 'react-router-dom';
+import { Settings2 } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 import UniversityHub from '@/components/university/UniversityHub';
+import UniversityAcademicAdministration from '@/components/university/UniversityAcademicAdministration';
 
-export default UniversityHub;
+export default function University() {
+  const [params] = useSearchParams();
+
+  if (params.get('view') === 'administration') {
+    return <UniversityAcademicAdministration />;
+  }
+
+  return (
+    <>
+      <UniversityHub />
+      <div className="fixed bottom-5 right-5 z-40">
+        <Button asChild variant="secondary" className="shadow-lg">
+          <Link to="/university?view=administration">
+            <Settings2 className="mr-2 h-4 w-4" />
+            Academic administration
+          </Link>
+        </Button>
+      </div>
+    </>
+  );
+}
